50fd30baa70a60deddc2f326a220ff5db0baef95
Josh <josh@observables.com>
2018-11-21 00:08:09 -0800

    Attempting to open port. Keep getting "ConnectionRefusedError".  No es bueno.

--- python/Test_Equipment_Control/Oscilloscope_test.py
+++ python/Test_Equipment_Control/Oscilloscope_test.py
@@ -5,7 +5,7 @@ Function: This code was written to test the remote control functionality of
 an oscilloscope, specifically, the Rigol DS1054Z. It uses the telnetlib library
 to send SCPI commands to the ip address defined in the Defines section, which
 can be changed to suit a compatible oscilloscope. This code can also be adapted
-to control other instruments over ethernet using SCPI
+to control other instruments over ethernet using SCPI.
 """
 
 #-------------------------------------------------------------------------------
@@ -16,8 +16,17 @@ import telnetlib
 #-------------------------------------------------------------------------------
 #------------------------------- Defines ---------------------------------------
 #-------------------------------------------------------------------------------
-oscilloscopeIP = "fe80::477:e0e0:bc52:e628%61" #change for different instrument
+oscilloscopeIPv6 = "fe80::477:e0e0:bc52:e628%61" #change for different instrument
+oscilloscopeIPv4 = "192.168.1.80"
 
 #-------------------------------------------------------------------------------
 #-------------------------------- Main -----------------------------------------
 #-------------------------------------------------------------------------------
+try:
+    with telnetlib.Telnet(oscilloscopeIPv4, 23) as oscilloscope:
+        oscilloscope.write(":MEASure:ADISplay 1\n")
+        oscilloscope.write(":MEASure:ADISPlay?\n")
+        print(oscilloscope.read_all())
+
+except ConnectionRefusedError as err:
+    print("Target refused connection")
