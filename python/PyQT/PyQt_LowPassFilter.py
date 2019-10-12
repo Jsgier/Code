18c6d3c0e34954c935c40a034599c890b8e73d32
Josh <joshuasgier@gmail.com>
2019-10-12 15:54:41 -0700

    Got button to output button clicked to terminal when pressed

--- python/PyQT/PyQt_LowPassFilter.py
+++ python/PyQT/PyQt_LowPassFilter.py
@@ -1,11 +1,43 @@
-from PyQt5.QtWidgets import QApplication, QLabel, QPushButton, QtWidgets
 
-app = QApplication([]) #one instance necessary in all PyQT applications
-widgets = QtWidgets
-app.setStyle('Fusion')
-label = QLabel('Hello World!')
-label.show()
+import sys
+from PyQt5.QtWidgets import QApplication, QLabel, QPushButton, QWidget
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import pyqtSlot
 
-#Add a 'solve' button
-solveButton = QPushButton('Filter Solved')
-app.exec_()
+class App(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.title = "Low Pass Filter"
+        self.left = 10
+        self.top = 10
+        self.width = 320
+        self.height = 200
+        self.initUI()
+        
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+
+        button = QPushButton('Solve Filter', self)
+        button.setToolTip('Solve the equation for the low pass filter')
+        button.move(100, 70)
+        button.clicked.connect(self.on_click)
+
+        self.show()
+    
+    @pyqtSlot()
+    def on_click(self):
+        print("Button CLicked")
+        #do math here later
+        #Add two input buttons for resistor and capacitor
+
+def bttnstate(self):
+    if self.solveButton.isChecked():
+        print ("Button Pressed")
+    else:
+        print("Button Released")
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = App()
+    sys.exit(app.exec_())
